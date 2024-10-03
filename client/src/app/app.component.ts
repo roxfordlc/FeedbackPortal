51f881072c510508f7c8d275d6bafdfe3adada92
Ryan Oxford <roxfordlc@gmail.com>
2024-10-03 14:48:28 -0400

    Created angluar client Configured CORS

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  http = inject(HttpClient);
+  title = 'Feedback Portal';
+  users: any
+  ngOnInit(): void {
+    this.http.get('https://localhost:5176/api/users').subscribe({
+      next: users => this.users = users,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    });
+  }
+}
